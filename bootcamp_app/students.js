5f1b4d611a22cbf4f66c4e748da0703ce96cc66c
ChrisPytel <chris_pytel@hotmail.com>
2024-04-18 16:16:56 -0400

    initialize students.js with new psql pool connection, and add package-lock.json

---
+++ bootcamp_app/students.js
@@ -0,0 +1,10 @@
+//initial setup for our node-postgres module to connect with the psql database we've created
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "development",
+  password: "development",
+  host: "localhost",
+  database: "bootcampx",
+});
+
